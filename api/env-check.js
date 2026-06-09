499d8e21a305435b1e4b2d30b3ff1b28ad21577a
MarcosRoa <136514427+MarcosRoa@users.noreply.github.com>
2026-06-09 16:48:15 -0300

    Delete api/env-check.js

--- api/env-check.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-    res.status(200).json({
-        has_url: !!process.env.SUPABASE_URL,
-        has_key: !!process.env.SUPABASE_SERVICE_ROLE_KEY,
-        node_version: process.version,
-        env: process.env.NODE_ENV
-    });
-}
