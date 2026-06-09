64eb9fd08ead664eaa868a995f751e4c0f5e59bd
MarcosRoa <136514427+MarcosRoa@users.noreply.github.com>
2026-06-09 16:18:10 -0300

    Create constants.js

---
+++ _lib/constants.js
@@ -0,0 +1,15 @@
+// api/_lib/constants.js
+
+// Email do usuário PRO vitalício
+export const PRO_FIXED_EMAIL = 'mresquadriasaluminio@gmail.com';
+
+// Créditos
+export const DEFAULT_CREDITS = 5;
+export const PRO_CREDITS = 100;
+export const COST_PER_GAME = 3;
+export const PRO_COST_PER_GAME = 2;
+
+// Configurações
+export const JOGO_PRICE = 3;
+export const CREDITOS_INICIAIS = 5;
+export const PRO_CREDITOS_FIXO = 100;
